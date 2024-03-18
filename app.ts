1dd54b3cf3cb62839e9899471e1062489c63b277
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-18 09:49:11 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,21 @@
+// Comparison Operators:
+
+//  Equal:
+let a = 20;
+let b = 20;
+console.log(a == b);
+
+//  Not Equal:
+let a1 = 10;
+let b1 = 20;
+console.log(a1 != b1);
+
+//  Greater Than:
+let a2 = 30;
+let b2 = 20;
+console.log(a2 > b2);
+
+//  Lass Than:
+let a3 = 20;
+let b3 = 30
+console.log(a3 < b3);
